266905422fa7f581b91ffaa1ea14e15b22b63328
Ben Waddell <benjamin.waddell@gmail.com>
2025-01-19 20:45:16 -0500

    Initial commit

---
+++ Assignment1/Assignment1Tests/Assignment1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1Tests.swift
+//  Assignment1Tests
+//
+//  Created by Ben Waddell on 1/19/25.
+//
+
+import Testing
+@testable import Assignment1
+
+struct Assignment1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
